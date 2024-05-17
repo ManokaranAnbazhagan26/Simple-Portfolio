4f97b50f68415bd7ed4ca613dc15a5868ce496d9
saurabhp94 <166574015+saurabhp94@users.noreply.github.com>
2024-05-17 01:14:03 -0400

    changss for small screens

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,54 +4,6 @@ import Fade from 'react-reveal';
 import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
 import { Container, Row, Col } from 'react-bootstrap';
-import "../css/darkbackground.css";
-
-const styles = {
-  nameStyle: {
-    fontSize: '5em',
-    '@media (max-width: 768px)': {
-      fontSize: '3em',
-    },
-  },
-  inlineChild: {
-    display: 'inline-block',
-  },
-  mainContainer: {
-    height: '100%',
-    display: 'flex',
-    padding: '5rem',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '@media (max-width: 768px)': {
-      padding: '2rem',
-    },
-  },
-  profilePic: {
-    width: '200px',
-    height: '200px',
-    borderRadius: '50%',
-    objectFit: 'cover',
-    marginBottom: '20px',
-    '@media (max-width: 768px)': {
-      width: '150px',
-      height: '150px',
-    },
-  },
-  paragraph: {
-    fontSize: '1.2rem',
-    lineHeight: '1.5',
-    fontFamily: 'Georgia, serif',
-    '@media (max-width: 768px)': {
-      fontSize: '1rem',
-      lineHeight: '1.3',
-    },
-  },
-  gap: {
-    marginTop: '0.75rem',
-    marginBottom: '2rem',
-  },
-};
 
 function Home() {
   const [data, setData] = useState(null);
@@ -66,13 +18,12 @@ function Home() {
   }, []);
 
   return data ? (
-    
     <Container>
       <div id="stars" />
-    <div id="stars2" />
-    <div id="stars3" />
+      <div id="stars2" />
+      <div id="stars3" />
       <Fade>
-        <div id="/" style={styles.mainContainer}>
+        <div id="/" className="mainContainer">
           {data.profilePic && (
             <img
               src={data.profilePic.source}
@@ -86,9 +37,9 @@ function Home() {
               }}
             />
           )}
-          <h1 style={styles.nameStyle}>{data.name}</h1>
+          <h1 className="nameStyle">{data.name}</h1>
           <div style={{ flexDirection: 'row' }}>
-            <h2 style={styles.inlineChild}>I'm </h2>
+            <h2 className="inlineChild">I'm </h2>
             <span>&nbsp;</span>
             <Typewriter
               options={{
@@ -98,12 +49,12 @@ function Home() {
               }}
             />
           </div>
-          <div style={styles.gap} />
+          <div className="gap" />
           {data.paragraph && (
             <Row className="justify-content-center">
               <Col md={10}>
                 {data.paragraph[0].split('. ').map((p, index) => (
-                  <p key={index} style={styles.paragraph}>
+                  <p key={index} className="paragraph">
                     {p}.
                   </p>
                 ))}
